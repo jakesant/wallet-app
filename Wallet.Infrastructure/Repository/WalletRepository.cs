b3c002dbddcea2c66cd2d3587803ab9ff189fde6
Jake Sant <jake.sant.18@um.edu.mt>
2025-08-21 20:51:07 +0200

    Wrote repo class for wallet entity

---
+++ Wallet.Infrastructure/Repository/WalletRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wallet.Infrastructure.Data;
+using Wallet.Infrastructure.Entities;
+
+namespace Wallet.Infrastructure.Repository
+{
+    public class WalletAccountRepository
+    {
+        private readonly AppDbContext _db;
+
+        public WalletAccountRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<WalletAccount?> GetByIdAsync(long id, CancellationToken cancellationToken = default)
+        {
+            return await _db.WalletAccounts.FindAsync([id], cancellationToken);
+        }
+
+        public async Task CreateAsync(WalletAccount wallet, CancellationToken cancellationToken = default)
+        {
+            _db.WalletAccounts.Add(wallet);
+            await _db.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task UpdateAsync(WalletAccount wallet, CancellationToken cancellationToken = default)
+        {
+            _db.WalletAccounts.Update(wallet);
+            await _db.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
