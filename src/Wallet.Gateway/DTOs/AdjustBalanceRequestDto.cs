0aa3380677ea3e7afb92d13c2f7ec43306e8efa8
Jake Sant <jake.sant.18@um.edu.mt>
2025-08-22 13:04:34 +0200

    Dtos for wallet and for updating it

---
+++ src/Wallet.Gateway/DTOs/AdjustBalanceRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet.Gateway.DTOs
+{
+    public class AdjustBalanceRequestDto
+    {
+        public decimal Amount { get; set; }
+        public string Currency { get; set; } = string.Empty;
+        public string Strategy { get; set; } = string.Empty;
+    }
+}
---
+++ src/Wallet.Gateway/DTOs/WalletDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet.Gateway.DTOs
+{
+    public class WalletDto
+    {
+        public WalletDto(long id, decimal balance, string currency)
+        {
+            Id = id;
+            Balance = balance;
+            Currency = currency;
+        }
+
+        public long Id { get; set; }
+        public string Currency { get; set; } = null!;
+        public decimal Balance { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime UpdatedAtUtc { get; set; }
+    }
+}
